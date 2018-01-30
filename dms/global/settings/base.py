4a10d3dc458ae3de15d2adefa8ed09300cfea78c
Kevin Dice <kevin.dice1@gmail.com>
2018-01-30 13:41:15 -0600

    Swap to port 80 and add domain name to list

--- dms/global/settings/base.py
+++ dms/global/settings/base.py
@@ -26,7 +26,7 @@ SECRET_KEY = Secret.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hosted.beocat.ksu.edu']
 
 
 # Application definition
