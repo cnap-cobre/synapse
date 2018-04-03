df1866ac6b3502330031fb814964c82c711ead37
Kevin Dice <kevin.dice1@gmail.com>
2018-04-03 17:01:12 -0500

    Use console email driver

--- dms/global/settings/dev.py
+++ dms/global/settings/dev.py
@@ -4,6 +4,7 @@ DEBUG=True
 INSTALLED_APPS = ['debug_toolbar'] + INSTALLED_APPS
 MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
 INTERNAL_IPS = ['localhost', '127.0.0.1', '172.18.0.1', '172.18.0.3']
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 #ALLOWED_HOSTS += ['dms']
 
 
