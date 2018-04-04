481efdcb87c0e607a574c70a4f7a7787a003f944
Kevin Dice <kevin.dice1@gmail.com>
2018-04-03 20:01:45 -0500

    Force debug toolbar to display in dev mode

--- dms/global/settings/dev.py
+++ dms/global/settings/dev.py
@@ -5,6 +5,14 @@ INSTALLED_APPS = ['debug_toolbar'] + INSTALLED_APPS
 MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
 INTERNAL_IPS = ['localhost', '127.0.0.1', '172.18.0.1', '172.18.0.3']
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+def show_toolbar(request):
+    return True
+
+DEBUG_TOOLBAR_CONFIG = {
+    "SHOW_TOOLBAR_CALLBACK" : show_toolbar,
+}
+
 #ALLOWED_HOSTS += ['dms']
 
 
