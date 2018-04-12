f06927bd75c941acff4248dd3ff71bbce94279ba
Kevin Dice <kevin.dice1@gmail.com>
2018-04-12 03:20:17 -0500

    Remove frontend attempt

--- frontend/Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM node:8.11.1
-
-RUN mkdir /frontend
-WORKDIR /frontend
-ADD . /frontend/
-RUN npm install
--- frontend/src/js/index.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load', function() {
-    const p = document.createElement('p');
-    p.innerHTML = 'hello world';
-    document.body.insertBefore(p, document.body.firstChild);
-});
