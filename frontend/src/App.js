8edb8186c793ec457064760aa65007a4011e1cf4
Kevin Dice <kevin.dice1@gmail.com>
2018-04-12 20:23:19 -0500

    Working towards embedded react layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Do you like cake?
+        </p>
+        <p>Yes</p>
       </div>
     );
   }
