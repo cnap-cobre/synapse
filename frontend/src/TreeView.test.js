387f4c041cd43acdf0db1ed66a2c8b4f172380b7
Kevin Dice <kevin.dice1@gmail.com>
2018-04-20 11:26:40 -0500

    Add trivial test for TreeView #7

---
+++ frontend/src/TreeView.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TreeView from './TreeView';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<TreeView />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
