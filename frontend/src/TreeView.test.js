30b7b2d29bc60922f3a3af0ba47916ff09d6f696
Kevin Dice <kevin.dice1@gmail.com>
2018-05-01 11:22:21 -0500

    Remove all webpack 3 stuff

--- frontend/src/TreeView.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TreeView from './TreeView';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<TreeView />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
