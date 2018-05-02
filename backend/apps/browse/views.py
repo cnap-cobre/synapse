61ce64b30d99c6985c9d8999f0e80748937238bd
Kevin Dice <kevin.dice1@gmail.com>
2018-05-01 19:12:45 -0500

    Revmove dependency on agavepy

--- backend/apps/browse/views.py
+++ backend/apps/browse/views.py
@@ -2,24 +2,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from allauth.socialaccount.models import SocialAccount, SocialToken
 from django.shortcuts import render
-from agavepy.agave import Agave, Token
 from apps.profile.models import Profile
-
-@login_required
-def browse_beocat_home(request):
-    user = request.user
-    try:
-        sa = SocialAccount.objects.filter(user=user, provider='agave').get()
-        try:
-            st = SocialToken.objects.filter(account=sa).get().token
-            ag = Agave(token=st,
-                    api_server='https://public.agaveapi.co')
-            files = ag.files.list(filePath='', systemId='beocat', limit=250, offset=0)
-        except SocialToken.DoesNotExist:
-            files = []
-    except SocialAccount.DoesNotExist:
-        files = []
-    return render(request, 'browse/browse.html', {'files': files})
 
 @login_required
 def react_browse(request):
