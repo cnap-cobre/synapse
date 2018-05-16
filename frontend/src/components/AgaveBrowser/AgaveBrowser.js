fe6dd84c7bb732fe896233fd8188344a5498c7b7
Kevin Dice <kevin.dice1@gmail.com>
2018-05-15 21:51:47 -0500

    Code format cleanup

--- frontend/src/components/AgaveBrowser/AgaveBrowser.js
+++ frontend/src/components/AgaveBrowser/AgaveBrowser.js
@@ -1,9 +1,7 @@
-import React from "react";
-import path from 'path';
+import React, { Component } from "react";
 import { humanFileSize } from "Utils/FileSize.js";
-
-import { Breadcrumb } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import PropTypes from 'prop-types';
+import {history} from 'react-router-dom';
 
 import FieldFieldHeader from "Components/FileFieldHeader/FileFieldHeader";
 import FileList from "Components/FileList/FileList";
@@ -11,45 +9,52 @@ import ErrorMessage from "Components/ErrorMessage/ErrorMessage";
 import Loader from "Components/Loader/Loader";
 import FileBreadcrumbs from "Components/Breadcrumbs/Breadcrumbs";
 
-export default class AgaveBrowser extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      list: [],
-      loading: true,
-      error: false,
-      errorMessage: ""
-    }
-  }
+export default class AgaveBrowser extends Component {
+  state = {
+    list: [],
+    loading: true,
+    error: false,
+    errorMessage: ""
+  };
+
+  static propTypes = {
+    prefix: PropTypes.string.isRequired,
+    system: PropTypes.string.isRequired,
+    systemDisplayName: PropTypes.string.isRequired,
+    history: PropTypes.shape({
+      length: PropTypes.number.isRequired,
+      location: PropTypes.object.isRequired,
+      action: PropTypes.string.isRequired,
+    }).isRequired
+  };
 
   componentDidMount() {
+    /*
+    * 1. Register a listener for browser location changes to update UI accordingly
+    * 2. Setup AbortController so that we can send a signal to cancel pending but
+    *    outdated requests.
+    * 3. Fetch the files for the current path.
+    * */
+
     const history = this.props.history;
-
-    this.abortController = new AbortController();
-
-
-    this.FetchFiles();
     this.stopListeningToHistoryChange = history.listen((location, action) => {
       this.abortController.abort();
       this.abortController = new AbortController();
       this.FetchFiles();
     });
+
+    this.abortController = new AbortController();
+    this.FetchFiles();
   }
 
   componentWillUnmount() {
     this._unmounted = true;
 
-    // Call to kill listener
+    // History location listener
     this.stopListeningToHistoryChange();
 
     // Kill any existing fetch calls
     this.abortController.abort();
-  }
-
-  getPath() {
-    return this.props.history.location.pathname.slice(
-        this.props.prefix.length
-    ).split('/').slice(1).slice(0, -1);
   }
 
   FetchFiles() {
@@ -58,40 +63,38 @@ export default class AgaveBrowser extends React.Component {
       this.setState({list: [], loading: true, error: false, errorMessage: ""});
     }
 
-    const filePath = [this.props.system, ...this.getPath()];
-
-    const url = '/agave/files/v2/listings/system/' + filePath.join('/');
+    const filePath = [this.props.system, ...this.getPath()].join('/');
+    const url = '/agave/files/v2/listings/system/' + filePath;
 
     fetch(url, {
       credentials: "same-origin",
       signal: this.abortController.signal
-    }).then((res) => {
-      this.request = null;
-      return res;
-    }).then((response) => {
-      // Throw a proper error
-      if(!response.ok) {
-        if (this.isMounted()) {
-          this.setState({errorMessage: response.statusText});
-        }
-        throw Error(response.statusText);
-      }
-      return response;
-    }).then((response) => {
-      return response.json();
-    }).then(({ result }) => {
-      this.setState({
-        list: result.filter(e => e.name !== '.'),
-        loading: false,
-        error: false
-      });
-    }).catch(( error ) => {
-      if (error.name === "AbortError") {
-        // no-op
-      } else if (!this._unmounted) {
-        this.setState({error: true, loading: false});
-      }
-    });
+    })
+        .then((res) => {
+          // Throw a proper error for a 500, etc. response code
+          if(!res.ok) throw Error(res.statusText);
+          return res;
+        })
+        .then((res) => {
+          // Convert to JSON
+          return res.json();
+        })
+        .then(({ result }) => {
+          // Update UI with result file list
+          this.setState({
+            list: result.filter(e => e.name !== '.'),
+            loading: false,
+            error: false
+          });
+        })
+        .catch(( error ) => {
+          // Handle errors
+          if (error.name === "AbortError") {
+            // no-op
+          } else if (!this._unmounted) {
+            this.setState({error: true, loading: false, errorMessage: error});
+          }
+        });
   }
 
   handleClick(item, e) {
@@ -99,6 +102,12 @@ export default class AgaveBrowser extends React.Component {
     if (item.format === 'folder' && item.name !== '.') {
       history.push('./' + item.name + '/');
     }
+  }
+
+  getPath() {
+    return this.props.history.location.pathname.slice(
+        this.props.prefix.length
+    ).split('/').slice(1).slice(0, -1);
   }
 
   render() {
