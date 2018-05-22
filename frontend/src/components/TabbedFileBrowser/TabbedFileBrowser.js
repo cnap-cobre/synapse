6ffe96d46bb7d28b8b1b1115ea287deaf607d994
Kevin Dice <kevin.dice1@gmail.com>
2018-05-21 23:46:09 -0500

    Generalize file browsers into config in tabbed file browser component

--- frontend/src/components/TabbedFileBrowser/TabbedFileBrowser.js
+++ frontend/src/components/TabbedFileBrowser/TabbedFileBrowser.js
@@ -1,22 +1,86 @@
 import React, {Component} from 'react';
 import {Tabs, Tab} from 'react-bootstrap';
 
+import {FaPlus} from 'react-icons/lib/fa';
+
 import AgaveBrowser from 'Components/AgaveBrowser/AgaveBrowser';
 
 import './fileTabs.css'
 
+const fileSystems = [
+  {
+    type: 'agave',
+    name: 'beocat',
+    displayName: 'Beocat'
+  },
+  {
+    type: 'agave',
+    name: 'viper',
+    displayName: 'Viper'
+  }
+];
+
+const addFileSystem = (
+    <span style={{
+      color: '#5dc56c'
+    }}>
+          Add File System
+          <FaPlus style={{
+            color: 'green',
+            position: 'relative',
+            left: '0.4em',
+          }}/>
+        </span>
+);
+
+
 export default class TabbedFileBrowser extends Component{
-  render(){
+  componentWillMount() {
+    const path = this.props.history.location.pathname;
+    const matches = [...fileSystems, {name:'new_file_system'}].map((fs)=>(path.indexOf(fs.name) !== -1))
+    if (matches.indexOf(true) === -1){
+      this.props.history.replace(fileSystems[0].name + '/');
+    }
+  }
+
+  browserMapper(system, index) {
     return (
-        <Tabs defaultActiveKey={1} id="FileBrowserTabs">
-          <Tab eventKey={1} title="Beocat">
-            <AgaveBrowser history={this.props.history}
-                          prefix="/files"
-                          system="beocat"
-                          systemDisplayName="Beocat" />
+        <Tab eventKey={index}
+             key={index}
+             title={system.displayName}>
+          <AgaveBrowser history={this.props.history}
+                        prefix={'/files/' + system.name}
+                        system={system.name}
+                        systemDisplayName={system.displayName} />
+        </Tab>
+    );
+  }
+
+  render(){
+    const urlActive = fileSystems.map(
+        (fs)=>(this.props.history.location.pathname.indexOf(fs.name) !== -1)
+    ).indexOf(true);
+    console.log(urlActive);
+    return (
+        <Tabs activeKey={(urlActive !== -1 ? urlActive : fileSystems.length)}
+              id="FileBrowserTabs"
+              animation={false}
+              onSelect={(key)=>{
+                console.log(key);
+                if (key === fileSystems.length){
+                  this.props.history.push('/files/new_file_system/');
+                } else {
+                  this.props.history.push(
+                      '/files/' + fileSystems[key].name + '/'
+                  );
+                }
+              }}>
+
+          {fileSystems.map(this.browserMapper.bind(this))}
+
+          <Tab eventKey={fileSystems.length} title={addFileSystem}>
+            Cake
           </Tab>
-          <Tab eventKey={2} title="Pizza">Pizza</Tab>
-          <Tab eventKey={3} title="Cake">Cake</Tab>
         </Tabs>
 
     );
