c55a91e51fe3b3c41714ba66c023d63083a71f3e
Kevin Dice <kevin.dice1@gmail.com>
2018-05-21 23:46:37 -0500

    Augment file icon association function

--- frontend/src/util/FileIconResolver.js
+++ frontend/src/util/FileIconResolver.js
@@ -3,9 +3,25 @@ import React from 'react';
 import {FaFolderO, FaFileO,
   FaFileWordO, FaFilePowerpointO, FaFileExcelO, FaFileImageO,
   FaFileMovieO, FaFileTextO, FaFileArchiveO, FaFilePdfO,
-  FaFileCodeO, FaFileAudioO, FaCogs} from 'react-icons/lib/fa';
+  FaFileCodeO, FaFileAudioO, FaCogs, FaTerminal} from 'react-icons/lib/fa';
+
+import {IoSocialPython, IoSocialJavascript,
+  IoSocialSass} from 'react-icons/lib/io';
+
+import {GoRuby, GoGitBranch, GoDatabase} from 'react-icons/lib/go';
 
 function resolve(item) {
+  if (item.type === "dir" && item.name.match(/^\.git$/i)) {
+    return (<span className="fa-layers fa-fw">
+      <FaFolderO/>
+      <GoGitBranch style={{
+        position: 'relative',
+        fontSize: '0.4em',
+        left: '-1.7em',
+        marginRight: '-1em'
+      }} />
+    </span>);
+  }
   if (item.type === "dir" && item.name.match(/^\./i)){
     return (<span className="fa-layers fa-fw">
       <FaFolderO/>
@@ -38,6 +54,12 @@ function resolve(item) {
   if (item.name.match(/\.(jpe?g|gif|bmp|tiff?|png|svg|eps|ai|ico)$/i)) {
     return (<FaFileImageO/>);
   }
+  if (item.name.match(/^\.(bash|zsh|ksh|tsh|csh|profile)/i)) {
+    return (<FaTerminal/>);
+  }
+  if (item.name.match(/^\.git/i)) {
+    return (<GoGitBranch/>);
+  }
   if (item.name.match(/^\./i)) {
     return (<FaCogs/>);
   }
@@ -47,10 +69,25 @@ function resolve(item) {
   if (item.name.match(/\.(wav|mp3|wma|m4a|acc|oga|flac|aiff|)$/i)) {
     return (<FaFileAudioO/>);
   }
-  if (item.name.match(/\.(asp|bash|c|class|cmd|cpp|cs|css|cxx|h|hdl|hpp|html|hxx|inc|java|jar|js|jsp|jsx|php|pl|pm|py|rb?|sh|sql|swift|tex|vb|xml)$/i)) {
+  if (item.name.match(/\.(asp|bash|c|class|cmd|cpp|cs|css|cxx|h|hdl|hpp|html|hxx|inc|java|jar|jsp|php|pl|pm|r|sh|swift|tex|vb|xml|zsh)$/i)) {
     return (<FaFileCodeO/>);
   }
-  if (item.name.match(/\.(txt|csv|tsv|log|md|rst)$/i)) {
+  if (item.name.match(/\.(sql)$/i)) {
+    return (<GoDatabase/>);
+  }
+  if (item.name.match(/\.(sass|scss)$/i)) {
+    return (<IoSocialSass/>);
+  }
+  if (item.name.match(/\.(js|jsx|json)$/i)) {
+    return (<IoSocialJavascript/>);
+  }
+  if (item.name.match(/\.py$/i)) {
+    return (<IoSocialPython/>);
+  }
+  if (item.name.match(/\.rb$/i)) {
+    return (<GoRuby/>);
+  }
+  if (item.name.match(/\.(txt|csv|tsv|log|md|rst|out)$/i)) {
     return (<FaFileTextO/>);
   }
   else {
