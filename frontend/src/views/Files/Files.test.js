823ef2349c76fad7975a110edba95a577bb6b264
Kevin Dice <kevin.dice1@gmail.com>
2018-05-25 14:29:30 -0500

    Add status code to mocked File list fetch call

--- frontend/src/views/Files/Files.test.js
+++ frontend/src/views/Files/Files.test.js
@@ -5,7 +5,7 @@ import Files from './Files';
 import ExampleFileListing from './ExampleFileListing.json'
 
 it('renders without crashing', ()=>{
-  fetch.mockResponseOnce(JSON.stringify(ExampleFileListing));
+  fetch.mockResponseOnce(JSON.stringify(ExampleFileListing), {status: 200});
 
   const div = document.createElement('div');
   ReactDOM.render(<MemoryRouter><Files/></MemoryRouter>, div);
