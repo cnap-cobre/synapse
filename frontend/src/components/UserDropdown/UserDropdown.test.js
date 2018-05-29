4315a4c66affc0189775470343249ac6fec26a5a
Kevin Dice <kevin.dice1@gmail.com>
2018-05-29 11:57:11 -0500

    Add mock router for user dropdown test

--- frontend/src/components/UserDropdown/UserDropdown.test.js
+++ frontend/src/components/UserDropdown/UserDropdown.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MockRouter from 'react-mock-router';
 import UserDropdown from './UserDropdown';
 
 it('renders without crashing', ()=>{
   const div = document.createElement('div');
-  ReactDOM.render(<UserDropdown/>, div);
+  ReactDOM.render(<MockRouter><UserDropdown/></MockRouter>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- frontend/src/views/Files/Files.test.js
+++ frontend/src/views/Files/Files.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {MemoryRouter} from 'react-router-dom';
+import MockRouter from 'react-mock-router';
 import Files from './Files';
 import ExampleFileListing from './ExampleFileListing.json'
 
@@ -8,6 +8,6 @@ it('renders without crashing', ()=>{
   fetch.mockResponseOnce(JSON.stringify(ExampleFileListing), {status: 200});
 
   const div = document.createElement('div');
-  ReactDOM.render(<MemoryRouter><Files/></MemoryRouter>, div);
+  ReactDOM.render(<MockRouter><Files/></MockRouter>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
