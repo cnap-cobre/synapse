ce6dacb0f542390a5ae37154857184bea80331f8
Kevin Dice <kevin.dice1@gmail.com>
2018-05-29 11:05:35 -0500

    Fix user dropdown

--- frontend/src/components/UserDropdown/UserDropdown.js
+++ frontend/src/components/UserDropdown/UserDropdown.js
@@ -1,44 +1,56 @@
 import React, {Component} from 'react';
+import {Collapse} from 'react-bootstrap';
 import face from '../../face-2.jpg';
 
-export default function UserDropdown() {
+export default class UserDropdown extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
+  render() {
     return (
         <div className="user">
-          <div className="photo">
-            <img src={face} alt="User Profile Photo"/>
-          </div>
           <div className="info">
-            <a data-toggle="collapse" href="#collapseExample" className="collapsed">
-              <span>
+            <div className="photo">
+              <img src={face} />
+            </div>
+
+            <a data-toggle="collapse"
+               onClick={() => {this.setState({open: !this.state.open})}}>
+	                        <span>
 								Your Name
-		            <b className="caret"></b>
+		                        <b className="caret"></b>
 							</span>
             </a>
             <div className="clearfix"></div>
 
-            <div className="collapse" id="collapseExample">
-              <ul className="nav">
-                <li>
-                  <a href="#profile">
-                    <span className="sidebar-mini">Mp</span>
-                    <span className="sidebar-normal">My Profile</span>
-                  </a>
-                </li>
-                <li>
-                  <a href="#edit">
-                    <span className="sidebar-mini">Ep</span>
-                    <span className="sidebar-normal">Edit Profile</span>
-                  </a>
-                </li>
-                <li>
-                  <a href="/account/logout/">
-                    <span className="sidebar-mini">L</span>
-                    <span className="sidebar-normal">Logout</span>
-                  </a>
-                </li>
-              </ul>
-            </div>
+            <Collapse in={this.state.open}>
+              <div>
+                <ul className="nav">
+                  <li>
+                    <a href="#profile">
+                      <span className="sidebar-mini">Mp</span>
+                      <span className="sidebar-normal">My Profile</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#edit">
+                      <span className="sidebar-mini">Ep</span>
+                      <span className="sidebar-normal">Edit Profile</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/account/logout/">
+                      <span className="sidebar-mini">L</span>
+                      <span className="sidebar-normal">Logout</span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </Collapse>
           </div>
         </div>
     );
+  }
 }
