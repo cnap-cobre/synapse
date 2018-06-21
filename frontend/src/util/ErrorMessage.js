a2a055d6f3df2117c7faed1e881d25ddd6167ece
Kevin Dice <kevin.dice1@gmail.com>
2018-06-21 18:36:23 -0500

    Clean up error message mapper

--- frontend/src/util/ErrorMessage.js
+++ frontend/src/util/ErrorMessage.js
@@ -1,23 +1,19 @@
 export function statusCodeToErrorMessage(error) {
   const code = error.status;
   const message = error.message;
+  const errorMessages = {
+    "400": "400 - Bad Request.  Something is wrong with how Synapse made this request to a 3rd party API.",
+    "401": "401 - Unauthorized.  You may need to sign in, or you may need to connect your Synapse account with a 3rd party account to access this service.",
+    "403": "403 - Forbidden.  You do not have permission to access this resource.  You may need to sign in or connect a 3rd party account to your Synapse account.",
+    "404": "404 - Not Found.  The requested resource cannot be found.",
+    "500": "500 - Internal Server Error.  The server encountered an unexpected condition which prevented it from filling the request.",
+    "501": "501 - Not Implemented.  The requested resource has not yet been implemented.  Contact the system administrator to voice interest in this feature.",
+    "502": "502 - Bad Gateway.  Something is wrong with the web server configuration.",
+    "503": "503 - Service Unavailable.  The requested resource is currently unavailable, perhaps due to regularly scheduled maintenance or overloading.  Please try again later."
+  };
 
-  if (code === 400) {
-    return "400 - Bad Request.  Something is wrong with how Synapse made this request to a 3rd party API.";
-  } else if (code === 401) {
-    return "401 - Unauthorized.  You may need to sign in, or you may need to connect your Synapse account with a 3rd party account to access this service.";
-  } else if (code === 403) {
-    return "403 - Forbidden.  You do not have permission to access this resource.  You may need to sign in or connect a 3rd party account to your Synapse account.";
-  } else if (code === 404) {
-    return "404 - Not Found.  The requested resource cannot be found.";
-  } else if (code === 500) {
-    return "500 - Internal Server Error.  The server encountered an unexpected condition which prevented it from filling the request.";
-  } else if (code === 501) {
-    return "501 - Not Implemented.  The requested resource has not yet been implemented.  Contact the system administrator to voice interest in this feature.";
-  } else if (code === 502) {
-    return "502 - Bad Gateway.  Something is wrong with the web server configuration.";
-  } else if (code === 503) {
-    return "503 - Service Unavailable.  The requested resource is currently unavailable, perhaps due to regularly scheduled maintenance or overloading.  Please try again later.";
+  if (code in errorMessages) {
+    return errorMessages[code];
   } else {
     return code + " - " + message;
   }
