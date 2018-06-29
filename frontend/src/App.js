37fb41fec81a46747bd77267e1ed818ab0f59e1b
Kevin Dice <kevin.dice1@gmail.com>
2018-06-29 02:45:51 -0500

    Move BrowserRouter to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
-import { HashRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import DefaultLayout from "./physical_layout/DefaultLayout/DefaultLayout";
 
 export default class App extends Component {
   render() {
     return (
-        <Switch>
-          <Route path="/" name="Home" component={DefaultLayout} />
-        </Switch>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" name="Home" component={DefaultLayout} />
+          </Switch>
+        </BrowserRouter>
     );
   }
 }
