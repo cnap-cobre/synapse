a64dddaac05b3bcbcf3d73d5e2a928adec30503a
Kevin Dice <kevin.dice1@gmail.com>
2018-06-29 12:21:51 -0500

    Add full_name to full.UserSerializer

--- backend/apps/user/api/serializers/full.py
+++ backend/apps/user/api/serializers/full.py
@@ -6,15 +6,20 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
         model = Group
         fields = ('id', 'name', 'url')
 
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     groups = GroupSerializer(many=True, read_only=True)
     is_staff = serializers.BooleanField(read_only=True)
+    full_name = serializers.SerializerMethodField()
+
+    def get_full_name(self, user):
+        return user.get_full_name()
 
     class Meta:
         model = User
-        fields = ('id', 'first_name', 'last_name', 'username',
-                  'email', 'groups', 'is_staff', 'is_active',
-                  'date_joined', 'url')
+        fields = ('id', 'first_name', 'last_name', 'full_name',
+                  'username', 'email', 'groups', 'is_staff',
+                  'is_active', 'date_joined', 'url')
         extra_kwargs = {
             'groups': {'lookup_field': 'pk'}
         }
