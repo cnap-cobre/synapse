d8b8aa2adb0e1feee662eee3b841e872d4383d3a
Kevin Dice <kevin.dice1@gmail.com>
2018-07-07 01:04:50 -0500

    fix theme javascript error

--- backend/apps/main/static/js/paper-dashboard.js
+++ backend/apps/main/static/js/paper-dashboard.js
@@ -56,7 +56,7 @@ $(document).ready(function(){
     });
 
     //  Activate the tooltips
-    $('[rel="tooltip"]').tooltip();
+    //$('[rel="tooltip"]').tooltip();
 
 
     $('.switch').bootstrapSwitch({
