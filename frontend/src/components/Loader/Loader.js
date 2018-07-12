b34eefc8b21dc5590a5e1314ff014057906eab04
Kevin Dice <kevin.dice1@gmail.com>
2018-07-12 03:27:07 -0500

    perf: optimize imports for react-spinners

--- frontend/src/components/Loader/Loader.js
+++ frontend/src/components/Loader/Loader.js
@@ -1,4 +1,4 @@
-import {PropagateLoader} from 'react-spinners';
+import PropagateLoader from 'react-spinners/dist/spinners/PropagateLoader';
 import PropTypes from 'prop-types';
 import React from "react";
 import './Loader.css';
