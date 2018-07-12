645baf3899785931b236be44b061bac851485c35
Kevin Dice <kevin.dice1@gmail.com>
2018-07-12 02:24:43 -0500

    chore: update prod settings

--- backend/global/settings/prod.py
+++ backend/global/settings/prod.py
@@ -5,4 +5,9 @@ DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1',
                  '172.18.0.1',
                  '127.18.0.3',
-                 'localhost']
+                 'localhost',
+                 'dataverse.cnap.ksu.edu',
+                 '172.18.0.4']
+
+CSRF_TRUSTED_ORIGINS += ['localhost']
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
