8989e25836171b4310d89685e083f523455df9f2
Kevin Dice <kevin.dice1@gmail.com>
2018-08-02 15:30:12 -0500

    Comment out nav for incomplete pages

--- frontend/src/nav.js
+++ frontend/src/nav.js
@@ -11,56 +11,56 @@ const nav = [
     icon: 'ti-folder',
     exact: false
   },
-  {
-    name: 'Code Editor',
-    url: '/editor',
-    icon: 'ti-paragraph',
-    exact: true
-  },
-  {
-    name: 'Compute Jobs',
-    url: '/compute',
-    icon: 'ti-cloud',
-    exact: true
-  },
-  {
-    name: 'Shell',
-    url: '/shell',
-    icon: 'ti-layout-cta-left',
-    exact: true
-  },
-  {
-    name: 'Desktop',
-    url: '/desktop',
-    icon: 'ti-layout-tab-window',
-    exact: false,
-    children: [
-      {
-        name: 'Jupyter Notebooks',
-        mini: 'J',
-        url: '/desktop/jupyter',
-        exact: true
-      },
-      {
-        name: 'Matlab',
-        mini: 'M',
-        url: '/desktop/matlab',
-        exact: true
-      },
-      {
-        name: 'Octave',
-        mini: 'O',
-        url: '/desktop/octave',
-        exact: true
-      },
-      {
-        name: 'XNAT',
-        mini: 'X',
-        url: '/desktop/xnat',
-        exact: true
-      }
-    ]
-  }
+  // {
+  //   name: 'Code Editor',
+  //   url: '/editor',
+  //   icon: 'ti-paragraph',
+  //   exact: true
+  // },
+  // {
+  //   name: 'Compute Jobs',
+  //   url: '/compute',
+  //   icon: 'ti-cloud',
+  //   exact: true
+  // },
+  // {
+  //   name: 'Shell',
+  //   url: '/shell',
+  //   icon: 'ti-layout-cta-left',
+  //   exact: true
+  // },
+  // {
+  //   name: 'Desktop',
+  //   url: '/desktop',
+  //   icon: 'ti-layout-tab-window',
+  //   exact: false,
+  //   children: [
+  //     {
+  //       name: 'Jupyter Notebooks',
+  //       mini: 'J',
+  //       url: '/desktop/jupyter',
+  //       exact: true
+  //     },
+  //     {
+  //       name: 'Matlab',
+  //       mini: 'M',
+  //       url: '/desktop/matlab',
+  //       exact: true
+  //     },
+  //     {
+  //       name: 'Octave',
+  //       mini: 'O',
+  //       url: '/desktop/octave',
+  //       exact: true
+  //     },
+  //     {
+  //       name: 'XNAT',
+  //       mini: 'X',
+  //       url: '/desktop/xnat',
+  //       exact: true
+  //     }
+  //   ]
+  // }
 ]
 
 export default nav;
