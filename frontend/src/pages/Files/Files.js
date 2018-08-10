2d416f5d2ea0b988ba05f0866a30e062719ec23c
Kevin Dice <kevin.dice1@gmail.com>
2018-08-10 04:11:06 -0500

    remove link for adding new file browser - can figure out a better spot for it later

--- frontend/src/pages/Files/Files.js
+++ frontend/src/pages/Files/Files.js
@@ -1,5 +1,4 @@
 import CardLayout from '../../physical_layout/CardLayout/CardLayout';
-import {Link} from 'redux-json-router';
 import React from 'react';
 import TabbedFileBrowser from "../../components/TabbedFileBrowser/TabbedFileBrowser";
 
@@ -8,13 +7,6 @@ export default class Files extends React.Component {
   render() {
     return (
         <CardLayout>
-          <Link to='/files/add_new_filesystem'
-                style={{
-                  position: 'absolute',
-                  right: '1.2em',
-                  top: '1.5em'
-                }}
-          >Add New System</Link>
           <TabbedFileBrowser prefix="/files" />
         </CardLayout>
     );
