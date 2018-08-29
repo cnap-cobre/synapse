21d134cc57c7763dd7adae5b612af098736e7edd
Kevin Dice <kevin.dice1@gmail.com>
2018-08-29 18:05:22 -0500

    More production and staging configuration

--- backend/global/settings/prod.py
+++ backend/global/settings/prod.py
@@ -6,8 +6,11 @@ ALLOWED_HOSTS = ['127.0.0.1',
                  '172.18.0.1',
                  '127.18.0.3',
                  'localhost',
-                 'dataverse.cnap.ksu.edu',
+                 'synapse.ksu.edu',
                  '172.18.0.4']
 
 CSRF_TRUSTED_ORIGINS += ['localhost']
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'apprelay.smtp.ksu.edu'
+DEFAULT_FROM_EMAIL = 'noreply@synapse.ksu.edu'
+
--- backend/global/settings/stage.py
+++ backend/global/settings/stage.py
@@ -1,5 +1,25 @@
 from .base import *
 from .secret import Secret
 
-DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', '172.18.0.1', '127.18.0.3', 'localhost']
+DEBUG = True
+ALLOWED_HOSTS = ['127.0.0.1',
+                 '172.18.0.1',
+                 '127.18.0.3',
+                 'localhost',
+                 'synapse.ksu.edu',
+                 '172.18.0.4']
+
+CSRF_TRUSTED_ORIGINS += ['localhost']
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'apprelay.smtp.ksu.edu'
+DEFAULT_FROM_EMAIL = 'noreply@synapse.ksu.edu'
+
+
+def show_toolbar(request):
+    return True
+
+DEBUG_TOOLBAR_CONFIG = {
+    "SHOW_TOOLBAR_CALLBACK": show_toolbar,
+}
+
+
