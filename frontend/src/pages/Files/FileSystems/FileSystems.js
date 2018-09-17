8995e3a3e334bd06953caa91a475ff9831116900
Kevin Dice <kevin.dice1@gmail.com>
2018-09-17 16:34:51 -0500

    Add instructions for file system adding

--- frontend/src/pages/Files/FileSystems/FileSystems.js
+++ frontend/src/pages/Files/FileSystems/FileSystems.js
@@ -70,7 +70,31 @@ export default class FileSystems extends React.Component {
                       <hr/>
                     </div>
                     <div className="card-content">
-                      Instructions go here.
+                      <p>
+                        There are two types of file systems which you can add to your
+                        Synapse account:  Agave-supported file systems and external file
+                        systems.
+                      </p>
+                      <p>
+                        Currently, Dropbox is the only external file system, but Google Drive,
+                        Globus, and Box maybe be added on request.
+                      </p>
+                      <hr />
+                      <h6>Dropbox</h6>
+                      <p>
+                        To add a Dropbox account, click the button above.  When prompted, enter your
+                        Dropbox credentials and grant Synapse permission to access your Dropbox.
+                        You may need to refresh to see the new system listed.
+                      </p>
+                      <hr />
+                      <h6>Agave Systems (via SFTP)</h6>
+                      <p>
+                        Fill out the "Add an SFTP File System" form.  You will need to generate an
+                        SSH key pair using ssh-keygen.  Add the public key to your ~/.ssh/authorized_keys
+                        file on the target file system.  Copy and paste each key, public AND private,
+                        into the form.  The system ID must be unique across all users, so it may help to
+                        suffix it with your username.
+                      </p>
                     </div>
                   </div>
                 </div>
