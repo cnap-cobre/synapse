a677e9f900ac52985e32a41704c3cbdb10652236
Kevin Dice <kevin.dice1@gmail.com>
2018-09-20 10:18:39 -0500

    Upload and download via Agave API with agavepy - Closes #76 - Closes #75

--- backend/apps/transfer/agave_adapter.py
+++ backend/apps/transfer/agave_adapter.py
@@ -1,9 +1,48 @@
 from allauth.socialaccount.models import SocialAccount, SocialToken
-
+from agavepy.agave import Agave
+import os
+from django.conf import settings
 
 class AgaveAdapter():
-    def download(self, path, user):
-        pass
+    def download(self, localPath, remotePath, user, uniqueBatchId):
+        token_query = user.profile.tokens.filter(app__provider='agave')
 
-    def upload(self, path, user):
-        pass
+        if not token_query.exists():
+            raise PermissionDenied('No agave token found.')
+
+        token = token_query.get().token
+        print(token)
+
+        systemId = remotePath.split('/')[1]
+        remoteFilePath = '/'.join(remotePath.split('/')[2:])
+        downloadDirectory = '/transient/%s/' % uniqueBatchId
+        fullLocalPath = downloadDirectory + localPath
+
+        if not os.path.exists(downloadDirectory):
+            os.makedirs(downloadDirectory)
+
+        ag = Agave(api_server=settings.API_BASE_URL_AGAVE, token=token)
+        response = ag.files.download(filePath=remoteFilePath, systemId=systemId)
+        f = open(fullLocalPath, 'wb')
+        f.write(response.content)
+        f.close()
+
+
+    def upload(self, localPath, remotePath, user, uniqueBatchId):
+        token_query = user.profile.tokens.filter(app__provider='agave')
+
+        if not token_query.exists():
+            raise PermissionDenied('No agave token found.')
+
+        token = token_query.get().token
+
+        systemId = remotePath.split('/')[1]
+        remoteFilePath = '/'.join(remotePath.split('/')[2:])
+        fullLocalPath = '/transient/%s/' % uniqueBatchId + localPath
+        print(remoteFilePath)
+
+        ag = Agave(api_server=settings.API_BASE_URL_AGAVE, token=token)
+        f = open(fullLocalPath, 'rb')
+        response = ag.files.importData(filePath='' + remoteFilePath, systemId=systemId, fileToUpload=f)
+        print(response)
+        f.close()
--- backend/apps/transfer/api/views.py
+++ backend/apps/transfer/api/views.py
@@ -8,6 +8,8 @@ from rest_framework.decorators import action
 from django.http import HttpResponse
 
 from ..dropbox_adapter import DropboxAdapter
+from ..agave_adapter import AgaveAdapter
+
 from .serializer import TransferBatchSerializer
 
 class TransferBatchViewSet(viewsets.ModelViewSet):
@@ -21,5 +23,14 @@ class TransferBatchViewSet(viewsets.ModelViewSet):
         print(request.path)
         dba = DropboxAdapter()
         #dba.download('/synapse-logo.png', '/synapse-logo.png', request.user, 'a134radfpizza')
-        #dba.upload('/easy-eagle-dvd-with-audiio-0.mp4', '/airplane/easy-eagle-dvd-with-audio-0.mp4', request.user, 'pizza')
+        #dba.upload('/easy-eagle-dvd-with-audiio-1.mp4', '/airplane/easy-eagle-dvd-with-audio-1.mp4', request.user, 'pizza')
         return HttpResponse('ASDF')
+
+    @action(methods=['get'], detail=False)
+    def agave_adapter_testing(self, request):
+        print(request)
+        print(request.path)
+        aga = AgaveAdapter()
+        #aga.download('/example.c', '/beocat-please-work/homes/kmdice/BrownianSimple3d/00-omp-naive.cpp', request.user, 'blue3')
+        aga.upload('testing123.txt', '/beocat-please-work/homes/kmdice/', request.user, 'blue4')
+        return HttpResponse('ASDFASDF')
