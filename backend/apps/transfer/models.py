bdc0f0273fc05248666958644236bc708051303f
Kevin Dice <kevin.dice1@gmail.com>
2018-09-20 10:27:05 -0500

    Store hash reference for transient storage deconfliction

--- backend/apps/transfer/models.py
+++ backend/apps/transfer/models.py
@@ -1,10 +1,17 @@
 from django.contrib.auth.models import User
 from django.db import models
-
+from datetime import datetime
+import hashlib
 
 class TransferBatch(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     dateInitiated = models.DateTimeField(auto_now_add=True)
+    hash = models.CharField(max_length=8, unique=True, default=None)
+
+    def save(self, *args, **kwargs):
+        if self.hash is None:
+            self.hash = hashlib.sha256(str(user) + str(datetime.now())).hexdigest()[0:8]
+        super().save(*args, **kwargs)
 
 
 class TransferFile(models.Model):
