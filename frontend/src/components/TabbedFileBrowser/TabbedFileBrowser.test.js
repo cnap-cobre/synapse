e2509edc931b37bc7226c856a863e1767cfd6ea6
Kevin Dice <kevin.dice1@gmail.com>
2018-10-15 15:35:09 -0500

    Update Browse and TabbedFileBrowser tests with initial state

--- frontend/src/components/TabbedFileBrowser/TabbedFileBrowser.test.js
+++ frontend/src/components/TabbedFileBrowser/TabbedFileBrowser.test.js
@@ -1,28 +1,41 @@
 import configureStore from 'redux-mock-store';
+import {initialAgaveFileSystemsState} from "../../reducers/agaveFileSystems";
 import { initialFileSystemsState } from "../../reducers/fileSystems";
 import { initialUserProfileState } from "../../reducers/userProfile";
 import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TabbedFileBrowser from './TabbedFileBrowser';
+import thunk from 'redux-thunk';
 
 
-const mockStore = configureStore([]);
+const mockStore = configureStore([thunk]);
 
 it('renders without crashing', ()=>{
   const div = document.createElement('div');
   ReactDOM.render((
       <Provider store={mockStore({
-        userProfile: initialUserProfileState,
+        agaveFileSystems: initialAgaveFileSystemsState,
         fileSystems: initialFileSystemsState,
+        files: {
+          "/agave/beocat/homes/asdf/": {
+            isFetching: false,
+            didInvalidate: false,
+            files: [],
+            hasFetched: true,
+            lastUpdated: 1539634558426,
+            symlinkCorrectionStarted: false
+          }
+        },
+        router: {
+          pathname: '/files/browse/agave/beocat/homes/asdf/',
+        },
+        userProfile: initialUserProfileState,
         visualOptions: {
           showDotfiles: true
         },
-        router: {
-          pathname: '/files/beocat/',
-        },
       })}>
-        <TabbedFileBrowser prefix="/files" />
+        <TabbedFileBrowser prefix="/files/browse" />
       </Provider>
   ), div);
   ReactDOM.unmountComponentAtNode(div);
--- frontend/src/pages/Files/Browse/Browse.test.js
+++ frontend/src/pages/Files/Browse/Browse.test.js
@@ -1,14 +1,16 @@
 import Browse from './Browse';
 import configureStore from 'redux-mock-store';
 import ExampleFileListing from './ExampleFileListing.json'
+import {initialAgaveFileSystemsState} from "../../../reducers/agaveFileSystems";
 import {initialFileSystemsState} from "../../../reducers/fileSystems";
 import {initialUserProfileState} from "../../../reducers/userProfile";
 import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import thunk from 'redux-thunk';
 
 
-const mockStore = configureStore([]);
+const mockStore = configureStore([thunk]);
 
 it('renders without crashing', ()=>{
   fetch.mockResponseOnce(JSON.stringify(ExampleFileListing), {status: 200});
@@ -16,17 +18,28 @@ it('renders without crashing', ()=>{
   const div = document.createElement('div');
   ReactDOM.render((
       <Provider store={mockStore({
+        agaveFileSystems: initialAgaveFileSystemsState,
+        fileSystems: initialFileSystemsState,
+        files: {
+          "/agave/beocat/homes/asdf/": {
+            isFetching: false,
+            didInvalidate: false,
+            files: [],
+            hasFetched: true,
+            lastUpdated: 1539634558426,
+            symlinkCorrectionStarted: false
+          }
+        },
+        focusedFiles: {
+          list: []
+        },
         router: {
           pathname: '/files/beocat/homes/kmdice/',
         },
+        userProfile: initialUserProfileState,
         visualOptions: {
           showDotfiles: true
         },
-        userProfile: initialUserProfileState,
-        fileSystems: initialFileSystemsState,
-        focusedFiles: {
-          list: []
-        }
       })}>
         <Browse/>
       </Provider>
