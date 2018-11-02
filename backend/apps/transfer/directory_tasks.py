14b2526b13f032982d024422863ef9a676ecb082
Kevin Dice <kevin.dice1@gmail.com>
2018-11-01 19:58:19 -0500

    Add directory handling to signal receiver #148

---
+++ backend/apps/transfer/directory_tasks.py
@@ -0,0 +1,9 @@
+from celery import shared_task
+from .models import TransferBatch, TransferFile
+
+
+@shared_task(bind=True, time_limit=60, default_retry_delay=10, max_retries=3)
+def expandDirectoryListing(self, fileId):
+    # This MUST be idempotent
+    print("THIS IS A DIRECTORY!!!!!")
+    pass
--- backend/apps/transfer/signals.py
+++ backend/apps/transfer/signals.py
@@ -2,10 +2,16 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 from .models import TransferBatch, TransferFile
 from apps.transfer.file_tasks import filePendingToDownloading
+from apps.transfer.directory_tasks import expandDirectoryListing
 
+# Trigger processing when TransferFile object is created
 @receiver(post_save, sender=TransferFile)
 def execute_after_save(sender, instance, created, *args, **kwargs):
     if created:
-        # Trigger processing when TransferFile object is created
-        print("File transfer object was created!")
-        filePendingToDownloading.delay(instance.id)
+        pathTokens = instance.fromPath.split('/')
+        if pathTokens[-1] == '':
+            # We have a directory path
+            expandDirectoryListing.delay(instance.id)
+        else:
+            # We have a file path
+            filePendingToDownloading.delay(instance.id)
