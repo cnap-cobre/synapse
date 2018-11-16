2228ee9fb8da15439acc84b5a6da8447579bdbc1
Kevin Dice <kevin.dice1@gmail.com>
2018-11-16 15:38:03 -0600

    Generate AgaveFileSystems reducer from higher order createReducer

--- frontend/src/store/agaveFileSystems/reducer.js
+++ frontend/src/store/agaveFileSystems/reducer.js
@@ -1,29 +1,23 @@
 import * as types from './types';
+import {createReducer} from "../utils";
 
 export const initialAgaveFileSystemsState = {
   systems: [],
   loading: true
 };
 
-export default function agaveFileSystems(state = initialAgaveFileSystemsState, action) {
-  switch (action.type) {
-    case types.GET_AGAVE_FILE_SYSTEMS_ASYNC.PENDING:
-      return {
-        ...state,
-        loading: true
-      };
-    case types.GET_AGAVE_FILE_SYSTEMS_ASYNC.SUCCESS:
-      return {
-        ...state,
-        loading: false,
-        systems: action.systems
-      };
-    case types.GET_AGAVE_FILE_SYSTEMS_ASYNC.ERROR:
-      return {
-        ...state,
-        loading: false
-      };
-    default:
-      return state;
-  }
-}
+export default createReducer(initialAgaveFileSystemsState, {
+  [types.GET_AGAVE_FILE_SYSTEMS_ASYNC.PENDING]: (state) => ({
+    ...state,
+    loading: true
+  }),
+  [types.GET_AGAVE_FILE_SYSTEMS_ASYNC.SUCCESS]: (state, action) => ({
+    ...state,
+    systems: action.systems,
+    loading: false
+  }),
+  [types.GET_AGAVE_FILE_SYSTEMS_ASYNC.ERROR]: (state, action) => ({
+    ...state,
+    loading: false,
+  })
+});
